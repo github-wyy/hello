817da62f5b98ba4f8f78b8d91c36c4bb88eb0dfb
wyy <202041566@qq.com>
2023-03-12 22:03:28 +0800

    暴露Hello函数

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func HelloTest(t *testing.T) {
 	want := "hello, world."
-	if got := hello(); got != want {
+	if got := Hello(); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
